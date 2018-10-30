b32f607942516a0299dbb5449214447f156d9c1d
Ben <leihouma@gmail.com>
2018-10-29 23:35:17 -0600

    added migration and console command to enable admin panel

---
+++ app/Console/Commands/EnableAdminPanel.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Console\Commands;
+
+use App\Models\User\User;
+use Illuminate\Console\Command;
+
+class EnableAdminPanel extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'admin:enable';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Enable admin panel for user';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        // Prompt for user_id
+        $user = $this->ask('What is the user\'s ID?');
+
+        $user = User::find($user);
+
+        // Show an error and exit if the user does not exist
+        if (!$user) {
+            $this->error('No user with that ID.');
+            return;
+        }
+
+        // Set user to admin
+        $user->is_admin = 1;
+        $user->save();
+
+        $this->info('User ' . $user->getNameAttribute() . ' is now an admin');
+    }
+}
---
+++ database/migrations/2018_10_30_050536_add_admin_toggle_to_users.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class AddAdminToggleToUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        if(!Schema::hasColumn('users', 'is_admin')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->boolean('is_admin')->default(0);
+            });
+        }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        if(Schema::hasColumn('users', 'is_admin')) {
+            Schema::table('users', function (Blueprint $table) {
+                $table->dropColumn('is_admin');
+            });
+        }
+    }
+}
